8218424810f4d93a3cea29b89b794f5003202cb2
Phong Ho <thaiphong.spkt@gmail.com>
2021-08-24 14:05:36 +0700

    added dynamodb mapper

--- libraries/database/src/repositories/user.repository.ts
+++ libraries/database/src/repositories/user.repository.ts
@@ -1,6 +1,8 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { Logger } from 'common';
 import { User } from '../entities';
+import {DataMapper} from '@aws/dynamodb-data-mapper';
+import DynamoDB = require('aws-sdk/clients/dynamodb');
 
 type GetUserParams = {
     userId: string;
@@ -26,9 +28,18 @@ export class UserRepository extends Repository<User> {
 
     const { userId } = params;
 
-    // return this.findOne({ id: userId });
+    const mapper = new DataMapper({
+      client: new DynamoDB({region: 'us-west-2'}), // the SDK client used to execute operations
+      tableNamePrefix: 'dev_' // optionally, you can provide a table prefix to keep your dev and prod tables separate
+  });
 
-    // TODO: MockData => need to be removed
+  mapper.get(Object.assign(new User, {id: 'foo', createdAt: new Date(946684800000)}))
+    .then(myItem => {
+      return Promise.resolve(myItem);
+    })
+    .catch(err => {
+        // the item was not found
+    })
 
     return Promise.resolve(null);
   }
